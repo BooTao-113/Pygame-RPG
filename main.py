27b8dd6e8de1b938499213b531dce1f76f33cebb
Faiz Shazwan <faizshazwan191@gmail.com>
2022-03-30 15:11:21 +0800

    Initialized Pygame Window

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import pygame, random, sys, time
 
+pygame.init()
+Clock = pygame.time.Clock()
+
+ScreenWidth = 600
+ScreenHeight = 600
+
+pygame.display.set_caption("Pygame RPG")
+game_window = pygame.display.set_mode((ScreenWidth,ScreenHeight))
+
+
+while True:
+
+    pygame.display.flip()
+    Clock.tick(60)
