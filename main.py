6ee85f6df4c3335df3b432ea7d3553880ecd25fb
Faiz Shazwan <faizshazwan191@gmail.com>
2022-03-31 17:49:52 +0800

    Rects

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import pygame, random, sys, time
 pygame.init()
 Clock = pygame.time.Clock()
 
-ScreenWidth = 600
-ScreenHeight = 600
+ScreenWidth = 1280
+ScreenHeight = 720
+tile_size = 40
 
 pygame.display.set_caption("Pygame RPG")
 game_window = pygame.display.set_mode((ScreenWidth,ScreenHeight))
+
+Player = pygame.Rect((ScreenWidth/2, ScreenHeight/2), (32, 32))
+Enemy = pygame.Rect((ScreenWidth/2, 600), (64, 64))
 
 # main loop
 while True:
